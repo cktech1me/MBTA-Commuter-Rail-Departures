5c903e89afc8a294cda598333de4861ca1b626bf
Sai-yu Ng <ngsaiyu@gmail.com>
2017-02-17 13:30:11 -0500

    Enable Heroku...

--- server.js
+++ server.js
@@ -15,18 +15,19 @@ let isInitialCompilation = true;
 
 const compiler = webpack(config);
 
+const herokuPort = process.env.PORT || config.port;
 const backendAPIServerConfig = {
   //proxy: {
-  //  '*': 'http://localhost:' + (config.port + 1) // <- backend
+  //  '*': 'http://localhost:' + (herokuPort + 1) // <- backend
   //}
 };
 
 const server = new WebpackDevServer(compiler, Object.assign({}, backendAPIServerConfig, config.devServer))
-  .listen(process.env.PORT || config.port, 'localhost', (err) => {
+  .listen(herokuPort, 'localhost', (err) => {
     if (err) {
       console.log(err);
     }
-    console.log('Listening at localhost:' + config.port);
+    console.log('Listening at localhost:' + herokuPort);
   });
 
 compiler.plugin('done', () => {
@@ -34,8 +35,8 @@ compiler.plugin('done', () => {
     // Ensures that we log after webpack printed its stats (is there a better way?)
     setTimeout(() => {
       console.log('\n✓ The bundle is now ready for serving!\n');
-      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/webpack-dev-server/');
-      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + config.port + '/\n');
+      console.log('  Open in iframe mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + herokuPort + '/webpack-dev-server/');
+      console.log('  Open in inline mode:\t\x1b[33m%s\x1b[0m', 'http://localhost:' + herokuPort + '/\n');
       console.log('  \x1b[33mHMR is active\x1b[0m. The bundle will automatically rebuild and live-update on changes.')
     }, 350);
   }
